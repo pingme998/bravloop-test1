b469dc47a012257c8d2b96b8ea1dc8082aabe924
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-29 18:24:23 +0530

    Update brav.sh

--- brav.sh
+++ brav.sh
@@ -7,9 +7,11 @@ rclone copy 1:brav.tar.gz / --config=/rclone.conf
 tar -xf /brav.tar.gz
 cp /brav/rclone /usr/bin/rclone
 chmod +x /usr/bin/rclone
-brave-browser-stable --no-sandbox --disable-dev-shm-usage --disable-gpu --no-default-browser-check --disable-client-side-phishing-detection --single-process --deterministic-mode --user-data-dir=/brav
+timeout -k 1500 1510 brave-browser-stable --no-sandbox --disable-dev-shm-usage --disable-gpu --no-default-browser-check --disable-client-side-phishing-detection --single-process --deterministic-mode --user-data-dir=/brav
 curl -L "$SELF"
 rm /brav.tar.gz
-tar -vcf /brav /brav.tar.gz
+tar -vcf /brav.tar.gz /brav
 rclone copy /brav.tar.gz 1: --config=/rclone.conf
+sleep 5
+curl -L "PASS"
 done
