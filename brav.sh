3419c5b46733f5baea7c4c085a5bd65d25f4fce4
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-29 23:28:08 +0530

    Update brav.sh

--- brav.sh
+++ brav.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-while :
-do
+#while :
+#do
 curl -L "$rclone_config_in_url" >/rclone.conf
 cd /
-rclone copy 1:brav.tar.gz / --config=/rclone.conf
-tar -xf /brav.tar.gz
-cp /brav/rclone /usr/bin/rclone
+#rclone copy 1:brav.tar.gz / --config=/rclone.conf
+#tar -xf /brav.tar.gz
+#cp /brav/rclone /usr/bin/rclone
 chmod +x /usr/bin/rclone
-timeout -k 1500 1510 brave-browser-stable --no-sandbox --disable-dev-shm-usage --disable-gpu --no-default-browser-check --disable-client-side-phishing-detection --single-process --deterministic-mode --user-data-dir=/brav
+#timeout -k 1500 1510 
+brave-browser-stable --no-sandbox --disable-dev-shm-usage --disable-gpu --no-default-browser-check --disable-client-side-phishing-detection --deterministic-mode --user-data-dir=/brav
 curl -L "$SELF"
 rm /brav.tar.gz
-tar -vcf /brav.tar.gz /brav
-rclone copy /brav.tar.gz 1: --config=/rclone.conf
-sleep 5
+therandom=$(date |sed 's/ /-/g')
+tar -vcf "/brav-$therandom.tar.gz" /brav
+rclone copy "/brav-$therandom.tar.gz" 1: --config=/rclone.conf
+echo "brav-$therandom.tar.gz" 
+sleep 5000
 curl -L "PASS"
-done
+#done
